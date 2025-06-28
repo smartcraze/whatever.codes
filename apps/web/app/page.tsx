53542913818620acb392b30130e7c76580ca95cd
suraj vishwakarma <smartcraze354@gmail.com>
2025-06-28 14:56:58 +0530

    widht managed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,25 @@
-import { Hero } from "@/components/hero"
-import { About } from "@/components/about"
-import { ProjectsShowcase } from "@/components/projects-showcase"
-import { InfraArchitecture } from "@/components/infra-architecture"
-import { BlogTeasers } from "@/components/blog-teasers"
-import { TechStack } from "@/components/tech-stack"
-import { Footer } from "@/components/footer"
+import { Hero } from "@/components/hero";
+import { About } from "@/components/about";
+import { ProjectsShowcase } from "@/components/projects-showcase";
+import { InfraArchitecture } from "@/components/infra-architecture";
+import { BlogTeasers } from "@/components/blog-teasers";
+import { TechStack } from "@/components/tech-stack";
+import { Footer } from "@/components/footer";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
       <Hero />
-      <About />
-      <ProjectsShowcase />
-      <InfraArchitecture />
-      <BlogTeasers />
-      <TechStack />
+
+      <main className="w-full max-w-6xl px-4 md:px-6 lg:px-8 mx-auto ">
+        <About />
+        <ProjectsShowcase />
+        <InfraArchitecture />
+        <BlogTeasers />
+        <TechStack />
+      </main>
+
       <Footer />
     </div>
-  )
+  );
 }
